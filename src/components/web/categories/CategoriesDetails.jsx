6076109146e02cb9dada9a9e53a14883928cd813
Osama7erzallah <osamaherzallah.2002@gmail.com>
2023-12-08 22:29:49 +0200

    with out ui

--- src/components/web/categories/CategoriesDetails.jsx
+++ src/components/web/categories/CategoriesDetails.jsx
@@ -1,9 +1,27 @@
+import axios from 'axios';
 import React from 'react'
+import { useQuery } from 'react-query';
+import {useParams}  from 'react-router-dom'
+
 
 function CategoriesDetails() {
-  return (
+    
+    const {category_id} = useParams();
+  
+  const getCategoriesDetails = async()=>{
+
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products/category/${category_id}`)
+      return data;
+
+  }
+  
+  const {data}= useQuery('categoriesDetails',getCategoriesDetails);
+  console.log(data);
+  
+  
+    return (
     <div>
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          {category_id}
     </div>
   )
 }
